b30d6fe615e6fc50c692f93508a168397b6ab776
Kasperdejong <kasperdejong@gmail.com>
2024-10-10 14:38:54 +0200

    About-us toegevoegd Controller aan routes aan view gelinked. Met Id gewerkt en werkende code in about-us en products na de / in de browser

--- _changelog/readme.md
+++ _changelog/readme.md
@@ -1,6 +1,9 @@
  # H1
   
-
+hou config dicht hoge kans dat die niet gebruikt hoeft te worden
+en NIKS doen met node_modules. Dit zijn gegenereerde bestanden
+voor de werking van de map.
+tests kunnen we negeren op deze opleiding NOOIT openen.
  * opsomming
  * sdfsds
  
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact/{id}', function(string $id) {
+    $info = 'DIT IS MIJN LINKEDIN: aaaaa';
+    return view ('contact', ['id' => $id, 'info' => $info]);
+})->whereNumber('id');
+
+route::get('/contact', function(){
+   return view ('contact');
+})->name('contact');
+
+route::resource('/about-us', AboutUsController::class);
+
+Route::resource('products', ProductController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
