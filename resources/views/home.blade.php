6fea610c299ba739c0c714e5d1705f3df5070a91
Kasperdejong <kasperdejong@gmail.com>
2024-10-14 12:38:06 +0200

    log in check werkt met @auth en @guest

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,12 @@
 
 <x-layout title="home">
 
+@auth()
+    je bent ingelogd trouwens hahaha
+ @endauth()
+    @guest()
+        jij bent niet ingelogd hoe durf je!
+    @endguest()
     <h1>Hallo homepage</h1>
 </x-layout>
 
